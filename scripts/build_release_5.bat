60856785c910d8dcb51e785008d6288a87dcbe83
SHM-white <1131287310a@gmail.com>
2023-09-09 11:17:41 +0800

    修改构建脚本

---
+++ scripts/build_release_5.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set QT_DIR=c:\Qt\5.15.2\msvc2019_64
+set SRC_DIR=%cd%
+set BUILD_DIR=%cd%\build_qt5
+
+if not exist %QT_DIR% exit
+if not exist %SRC_DIR% exit
+if not exist %BUILD_DIR% md %BUILD_DIR%
+
+cd build
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+%QT_DIR%\bin\qmake.exe %SRC_DIR%\TimeTable-Qt\TimeTable-Qt.pro -spec win32-msvc  "CONFIG+=release"
+if exist %BUILD_DIR%\release\TimeTable-Qt.exe del %BUILD_DIR%\release\TimeTable-Qt.exe
+nmake Release
+if not exist %BUILD_DIR%\release\Qt5Core.dll (
+  %QT_DIR%\bin\windeployqt.exe %BUILD_DIR%\release\TimeTable-Qt.exe
+)
